e4d11716603476c3c0fefe3d5d4304d643a9b481
Ztirom45 <moritz.busse.mb@web.de>
2025-06-14 10:47:12 +0200

    frontend sending data via usb works

--- frontend/main.py
+++ frontend/main.py
@@ -19,17 +19,35 @@ def get_devices():
             return
 
 
-def send_message():
-    # Find the USB device
-    device = usb.core.find(idVendor=1027, idProduct=24577)
+device = usb.core.find(idVendor=0x403, idProduct=0x6001)
 
-    # Set the device configuration
-    device.set_configuration()
+if device==None:
+    raise ValueError("Device not found")
+# Set the device configuration
+device.get_active_configuration()
 
-    # Send data to the device
-    endpoint = device[0][(0,0)][0]
-    data = b'add 0 0 0 0 0 2'
-    device.write(endpoint.bEndpointAddress, data)
 
-get_devices()
-#send_message()
+def send_command(cmd:bytes,endpoint):
+    len_cmd = len(cmd)
+    if(len_cmd > 64):
+        raise OverflowError("cmd can't be longer than 64 bytes")
+    global device
+    cmd_64_bytes = cmd + b'\x00' * (64 - len_cmd)#raises overflow error if it is not exacly 64 bytes
+    device.write(endpoint.bEndpointAddress, cmd_64_bytes)
+
+reattach = False
+if device.is_kernel_driver_active(0):
+    reattach = True
+    device.detach_kernel_driver(0)
+
+
+endpoint = device[0][(0,0)][0]
+
+
+send_command(b"add 45 45 45 45 180 1",endpoint)
+send_command(b"run",endpoint)
+
+if reattach:
+    device.attach_kernel_driver(0)
+
+#get_devices()
