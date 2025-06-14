014cbe26a5d57e34ef1423b0bb81c4fef42b876a
Ztirom45 <moritz.busse.mb@web.de>
2025-06-14 11:36:39 +0200

    add ready message to backend

--- frontend/main.py
+++ frontend/main.py
@@ -3,4 +3,6 @@ import serial
 arduino = serial.Serial("/dev/ttyUSB1",timeout=1, baudrate=9600)
 
 while True: 
-	print(arduino.readline().decode("utf-8"))
+    read_data = arduino.readline().decode("utf-8")
+    if(read_data != ""):
+        print(read_data)
